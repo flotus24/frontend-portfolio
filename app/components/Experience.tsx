3b5f063b4825f27f53cb08658bd706cc18d2a485
unknown <rifqi367@upi.edu>
2023-09-09 10:26:57 +0700

    fixing logo allignment in experience

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -37,7 +37,7 @@ const Experience = () => {
       id: 6,
       src: "/next.svg",
       title: "Next JS",
-      style: "shadow-white py-8"
+      style: "shadow-white"
     },
     {
       id: 7,
@@ -59,7 +59,9 @@ const Experience = () => {
 
           {techs.map(({ id, src, title, style }) => (
             <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <div className="h-20 flex">
+                <img src={src} alt="" className="w-20 mx-auto flex" />
+              </div>
               <p className="mt-4">{title}</p>
             </div>
           ))}
