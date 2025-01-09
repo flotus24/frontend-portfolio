6a07d3dd0de01e7f0d07aad02ba61012f3b01a74
flotus24 <rifqi367@gmail.com>
2025-01-09 12:26:38 +0700

    fixed portfolio image

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -1,32 +1,31 @@
 import React from "react"
 
 const Portfolio = () => {
-
   type Portfolio = {
-    id: number,
-    src: string,
-    link: string,
+    id: number
+    src: string
+    link: string
     desc: string
   }
 
   const portfolios: Portfolio[] = [
     {
       id: 1,
-      src: "/land_map.PNG",
-      link: "https://github.com/flotus24/Land-Map",
-      desc: "Land Map was made as a product of a thesis where it aims to centralize the mapping of the earth's surface, especially drought, on a website with the help of remote sensing."
+      src: "/jamazon.PNG",
+      link: "https://github.com/flotus24/DeteksiCovid",
+      desc: "Jamazon, an e-commerce website inspired by Amazon and Tokopedia, a platform that combines user-friendly design and vast product selection.",
     },
     {
       id: 2,
-      src: "/ocors.PNG",
-      link: "https://github.com/naufal58/OCORS/tree/main/OCORS",
-      desc: "OCORS stand for Online Comic Reader and Store is a website that our team developed to read and buy comic so reader can read and buy comic legally and easily through our portal."
+      src: "/land_map.PNG",
+      link: "https://github.com/flotus24/Land-Map",
+      desc: "Land Map was made as a product of a thesis where it aims to centralize the mapping of the earth's surface, especially drought, on a website with the help of remote sensing.",
     },
     {
       id: 3,
-      src: "/deteksicovid.PNG",
-      link: "https://github.com/flotus24/DeteksiCovid",
-      desc: "DeteksiCovid is a website that our team developed to inform the public community about Covid-19 and make an appointment and payment with a local hospital through the website to check Covid-19 with a Swab test or Rapid test.",
+      src: "/ocors.PNG",
+      link: "https://github.com/naufal58/OCORS/tree/main/OCORS",
+      desc: "OCORS stand for Online Comic Reader and Store is a website that our team developed to read and buy comic so reader can read and buy comic legally and easily through our portal.",
     },
   ]
 
@@ -45,24 +44,25 @@ const Portfolio = () => {
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 pb-20 sm:pb-0">
           {portfolios.map(({ id, src, link, desc }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg bg-black to-gray-800 bg-gradient-to-b">
-              <img
-                src={src}
-                alt=" "
-                className="rounded-md duration-200 hover:scale-105"
-              />
+            <div
+              key={id}
+              className="shadow-md lg:w-[300px] lg:h-[410px] shadow-gray-600 rounded-lg"
+            >
+              <div className="sm:h-[170px] md:h-[160px] content-center rounded-md">
+                <img
+                  src={src}
+                  alt=" "
+                  className="rounded-md duration-200 hover:scale-105 w-[100%] lg:w-[300px]"
+                />
+              </div>
               <div className="flex flex-col items-center justify-center ">
-                {/* <button className={`w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 ${style}`}>
-                  Demo
-                </button> */}
-                <div className="sm:h-[220px]">
+                <div className="sm:h-[175px]">
                   <p className="py-3 px-3">{desc}</p>
                 </div>
-                <div className={`hover:animate-pulse center m-4 duration-200 hover:scale-105 text-white w-fit px-3 py-1 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer`}>
-                  <a
-                    href={link}
-                    target="_blank"
-                  >
+                <div
+                  className={`hover:animate-pulse center m-4 duration-200 hover:scale-105 text-white w-fit px-3 py-1 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer`}
+                >
+                  <a href={link} target="_blank">
                     Code
                   </a>
                 </div>
